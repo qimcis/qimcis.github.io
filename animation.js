df3e6835dbf2028d095c3edee6cf1a8338299a5d
qimcis <153383231+qimcis@users.noreply.github.com>
2024-07-26 02:32:40 +0800

    website update

--- animation.js
+++ animation.js
@@ -1,29 +1,3 @@
-/* Animate text going from left to right */   
-
-const the_animation = document.querySelectorAll('.animation')
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('scroll-animation')
-        }
-        else {
-            entry.target.classList.remove('scroll-animation')
-        }
-    })
-},
-{threshold: 0.5
-
-});
-
-for (let i = 0; i < the_animation.length; i++) {
-    const elements = the_animation[i];
- 
-     observer.observe(elements);
-   } 
-
-/* Animate text going from right to left */   
-
 const the_animationleft = document.querySelectorAll('.animationleft')
 
 const observer2 = new IntersectionObserver((entries) => {
@@ -45,3 +19,70 @@ for (let i = 0; i < the_animationleft.length; i++) {
  
      observer2.observe(elements);
    } 
+
+
+let textBase = "i'm ";
+let jobs = ["a software engineer.", "a UI/UX designer.", "an artist.", "a problem solver.", "happy to meet you!"];
+let jobIdx = 0;
+let i = 0;
+let reverse = false;
+
+function typeWriter() {
+  // Generate some random text jitter between 45 and 75 ms to simulate a keyboard
+  var textJitter = Math.floor(Math.random() * (70 - 45) + 45);
+
+  // Check if we want to remove text ('reverse'), or add it.
+  if (reverse) {
+    if (document.getElementById("text").innerHTML.length > textBase.length) {
+      // We're still in the process of removing the job
+      document.getElementById("text").innerHTML = document
+        .getElementById("text")
+        .innerHTML.slice(0, -1);
+      setTimeout(typeWriter, textJitter);
+    } else {
+      // deleting done. Set next job, and repeat with typing by
+      // setting reverse to false
+      jobIdx = (jobIdx+1) % 5;
+      reverse = false;
+      setTimeout(typeWriter, 1000);
+    }
+  } else {
+    // We're adding text
+    if (i === (textBase + jobs[jobIdx]).length) {
+      // Line is done. Wait and then reverse
+      i = textBase.length;
+      reverse = true;
+
+      // Wait a second, then start deleting
+      setTimeout(typeWriter, 3000);
+    } else {
+      // Write text like a typewriter
+      if (i < (textBase + jobs[jobIdx]).length) {
+        document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + (
+          textBase + jobs[jobIdx]
+        ).charAt(i);
+        i++;
+        setTimeout(typeWriter, textJitter);
+      }
+    }
+  }
+}
+
+typeWriter();
+
+// scroll
+function smoothScroll(){
+  document.querySelector('#about').scrollIntoView({
+      behavior: 'smooth'
+  });
+}
+
+//email
+
+document.addEventListener('DOMContentLoaded', function() {
+  const contactButton = document.getElementById('contactButton');
+  
+  contactButton.addEventListener('click', function() {
+      window.location.href = "mailto:chixie.mcisaac@gmail.com";
+  });
+});
